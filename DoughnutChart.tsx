9d57c287efee2be9cc159fbc15356dcc8de1e110
gowrishankar123cmyk <75079421+gowrishankar123cmyk@users.noreply.github.com>
2024-06-03 19:31:15 +0530

    Add files via upload

---
+++ DoughnutChart.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountNames = accounts.map((a) => a.name);
+  const balances = accounts.map((a) => a.currentBalance)
+
+  const data = {
+    datasets: [
+      {
+        label: 'Banks',
+        data: balances,
+        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
+      }
+    ],
+    labels: accountNames
+  }
+
+  return <Doughnut 
+    data={data} 
+    options={{
+      cutout: '60%',
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }}
+  />
+}
+
+export default DoughnutChart
